8d223a4ee911bae82f9181f216fce396b73307f7
wowjdd133 <wowjdd133@naver.com>
2020-06-12 12:04:41 +0900

    [Create] Loading.tsx
    로딩 생성

---
+++ findMask/Loading.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ActivityIndicator, View, StyleSheet } from 'react-native';
+
+export default function Loading () {
+  return(
+    <View style={styles.container}>
+      <ActivityIndicator size="large" color="#000ff"/>  
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+})
