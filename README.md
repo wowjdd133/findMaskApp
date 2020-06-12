b6b14c9ce76e9f6f0a84d30e0d106bd22737558a
wowjdd133 <wowjdd133@naver.com>
2020-06-12 12:04:07 +0900

    [Create] struct
    기본적 구조

--- README.md
+++
@@ -1,2 +0,0 @@
-# findMaskApp
- react-native
---
+++ findMask/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Map from './Map';
+import {View} from 'react-native';
+
+export default function App() {
+  return (
+    <Map/>
+  );
+}
+
