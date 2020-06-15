55cd4076379b84bc6e5c5a5f2f54d6afe47fadee
wowjdd133 <wowjdd133@naver.com>
2020-06-15 20:33:17 +0900

    [Update] Map
    use query로 변경.
    map view, marker, 상수들 선언.

--- findMask/components/Map.tsx
+++ findMask/components/Map.tsx
@@ -1,79 +1,163 @@
-import React,{useState, useEffect} from 'react';
-import MapView from 'react-native-maps';
-import {StyleSheet, Text, Dimensions, Alert} from 'react-native'
-import * as Location from 'expo-location';
+import React from 'react';
+import gql from 'graphql-tag';
+import {StyleSheet, Text, Dimensions, Alert, Linking} from 'react-native'
+import {LocationType} from '../containers/Map';
+// import MarkerList from './MarkerList';
+import { useQuery } from '@apollo/react-hooks'
 import Loading from './Loading';
+import { FlatList,View,SafeAreaView } from 'react-native';
+import MapView,{Marker, Callout} from 'react-native-maps';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-interface LocationType {
-  coords?: {
-    accuracy: number,
-    altitude: number,
-    heading: number,
-    latitude: number,
-    longitude: number,
-    speed: number,
+const GET_MASK_DATA = gql`
+  query getMasks($input:MaskInput!){
+    Masks(input: $input){
+      count
+      stores{
+        code,
+        addr,
+        name,
+        lat,
+        lng,
+        remain_stat
+      }
+    }
+  }
+`;
+
+interface MaskInput {
+  input : {
+    lat: number,
+    lon: number,
+    m: number,
+  }
+};
+interface Stores {
+  name: string,
+  addr: string,
+  lat: number,
+  lng: number,
+  code: string,
+  remain_stat: string,
+}
+interface MaskData {
+  Masks:{
+    count: number,
+    stores: [Stores]
   }
 }
 
-const Map = () => {
-  const [location, setLocation] = useState<LocationType>({});
-  const [loading, setLoading] = useState<Boolean>(true);
-
-  useEffect(() => {
-    (async () => {
-      try{
-        setLoading(true);
-        let { status } = await Location.requestPermissionsAsync();
-        if (status !== 'granted'){
-          throw new Error; 
+const Map = (props:LocationType) => {
+    if(props.coords){
+      const { data, loading, error} = useQuery<MaskData, MaskInput>(GET_MASK_DATA, {
+        variables: {
+          input:{
+            lat: props.coords.latitude,
+            lon: props.coords.longitude,
+            m: 1000
+          }
         }
-
-        let location = await Location.getCurrentPositionAsync({});
-        
-        setLocation(location);
-        setLoading(false);
-        
-      } catch(err){
-        Alert.alert("실패","다시 시도하시겠습니까?",[{
-          text: "예",
-          onPress: () => console.log('1'),
-        },
-        {
-          text: "아니오",
-          onPress: () => console.log('2'),
-        }
-      ])
+      });
+  
+      if(loading) {
+        return <Loading/>
       }
-    })();
-    return() => {
-      setLoading(false);
-      setLocation({});
+    
+      if(error){
+        return <Text>error..</Text>
+      }
+  
+      if(data){
+        const { height, width } = Dimensions.get("window");
+        const LATITUDE_DELTA = 0.01501;
+        const LONGITUDE_DELTA = LATITUDE_DELTA * (width / height);
+        const MARKER_COLOR = {
+          plenty: "#40E0D0",
+          few: "#7CFC00",
+          some: "#FFA500",
+          empty: "#ff0000",
+          break: "#808080",
+        };
+        const REMAIN_STAT = {
+          plenty: "30개 이상 100개 이하",
+          few: "10개 이상 29개 이하",
+          some: "2개 이상 9개 이하",
+          empty: "0~1개",
+          break: "판매 중지",
+        }
+  
+        return (
+              <MapView
+                style={styles.mapStyle}
+                initialRegion={{
+                  latitude: props.coords.latitude,
+                  longitude: props.coords.longitude,
+                  latitudeDelta: LATITUDE_DELTA,
+                  longitudeDelta: LONGITUDE_DELTA
+                }}
+              >
+                  {data.Masks.stores.map((item:Stores) => {
+                    const color = MARKER_COLOR[item.remain_stat];
+                    const stat = REMAIN_STAT[item.remain_stat];
+  
+                    return(
+                      <Marker
+                        pinColor={color}
+                        key={item.code}
+                        coordinate={{latitude:item.lat, longitude:item.lng}}
+                      >
+                        <Callout onPress={() => {
+                          Alert.alert("웹페이지 연결","길찾기를 위해 웹페이지 'map.kakao.com'으로 연결합니다.",[
+                            {
+                              text: '예',
+                              onPress: () => {Linking.openURL(`https://map.kakao.com/link/to/${item.name},${props.coords.latitude},${props.coords.longitude}`)}
+                            },
+                            {
+                              text: '아니오',
+                              onPress: () => {console.log('아뇨')}
+                            }
+                          ])
+                        }}>
+                          <View style={styles.callout}>
+                            <Text style={styles.title}>{item.name}</Text>
+                            <Text style={styles.description}>{item.addr} {"\n"} {stat}</Text>
+                          </View>
+                        </Callout>
+                      </Marker>
+                    )
+                  })}
+              </MapView>
+        )
     }
-  },[])
-
-  return loading ? <Loading/> : 
-  <MapView
-    style={styles.mapStyle}
-    initialRegion={{
-      latitude: location.coords.latitude,
-      longitude: location.coords.longitude,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0832,
-    }}
-  />
+    }
+    
+  return <Text>hi</Text>
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ccc',
     alignItems: 'center',
     justifyContent: 'center',
   },
   mapStyle: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    flex: 1,
+    justifyContent: 'center'
+  },
+  callout:{
+    flex: 1,
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  description: {
+    fontSize: 12,
+    textAlign: "center"
   }
-})
+});
 
 export default Map;
