4299279e539b6b4fc699fc4f186aa49b9cd317b9
wowjdd133 <wowjdd133@naver.com>
2020-06-15 20:31:44 +0900

    [Update] client
    preset을 사용하는 것에서 error handling을 위해서 변경.

--- findMask/client.ts
+++ findMask/client.ts
@@ -1,8 +1,36 @@
-import { ApolloClient, HttpLink, InMemoryCache} from 'apollo-client-preset';
+// import { ApolloClient, HttpLink, InMemoryCache} from 'apollo-client-preset';
+import {ApolloLink, from} from 'apollo-link';
+import {ApolloClient} from 'apollo-client';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {HttpLink} from 'apollo-link-http';
+import {onError} from 'apollo-link-error';
 
-const client = new ApolloClient({
-  link: new HttpLink({ uri: 'http://192.168.35.17:8000/graphql'}),
-  cache: new InMemoryCache(),
+const API = new HttpLink({
+  uri: 'http://10.80.162.77:8000/graphql'
 });
 
-export default client;
+const cache = new InMemoryCache();
+
+const afterWareLink = onError(({operation, response, graphQLErrors = {}, networkError = {} as any}) => {
+  const status: number = networkError && networkError.statusCode ? networkError.statusCode : null;
+    console.log(operation);
+    console.log(response);
+    console.log(graphQLErrors);
+    console.log(status);
+});
+
+
+// const client = new ApolloClient({
+//   link: new HttpLink(
+//     { uri: 'http://10.80.162.77:8000/graphql'
+//   }),
+//   cache: new InMemoryCache(),
+// });
+
+export const apolloClient = new ApolloClient({
+  link: from([
+    afterWareLink,
+    API
+  ]),
+  cache: cache,
+});
