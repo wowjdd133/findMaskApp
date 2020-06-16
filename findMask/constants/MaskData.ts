d3962871cb513c96b04f4fe96489fc4230a999da
wowjdd133 <wowjdd133@naver.com>
2020-06-16 16:49:58 +0900

    [Create] MaskData
    mask data 관련 상수 담고 있는 파일 생성

---
+++ findMask/constants/MaskData.ts
@@ -0,0 +1,17 @@
+export const LATITUDE_DELTA = 0.01501;
+
+export const MARKER_COLOR = {
+  "plenty": "#40E0D0",
+  "few": "#7CFC00",
+  "some": "#FFA500",
+  "empty": "#ff0000",
+  "break": "#808080",
+};
+
+export const REMAIN_STAT = {
+  plenty: "100개 이하",
+  few: "29개 이하",
+  some: "9개 이하",
+  empty: "1개 이하",
+  break: "판매 중지",
+};
