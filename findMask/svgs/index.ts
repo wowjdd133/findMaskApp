93d46dd4ab2195ce31fddb13a2d6fdd8065af5de
wowjdd133 <wowjdd133@naver.com>
2020-06-17 19:30:45 +0900

    [Create] svg/ Board, Korea, Mask, Profile, World, index
    svg 파일들 추가

---
+++ findMask/svgs/index.ts
@@ -0,0 +1,13 @@
+import BoardIcon from './Board';
+import KoreaIcon from './Korea';
+import MaskIcon from './Mask';
+import ProfileIcon from './Profile';
+import WorldIcon from './World';
+
+export const Icons = {
+  BoardIcon,
+  KoreaIcon,
+  MaskIcon,
+  ProfileIcon,
+  WorldIcon
+};
