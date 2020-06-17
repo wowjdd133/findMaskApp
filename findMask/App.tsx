a5a9e35b3d30553871925d29088450bbb2d588d6
wowjdd133 <wowjdd133@naver.com>
2020-06-17 19:33:59 +0900

    [Update] App
    Navigation 분리.

--- findMask/App.tsx
+++ findMask/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import { AppRegistry, StyleSheet, View } from 'react-native';
+import { AppRegistry} from 'react-native';
 import { ApolloProvider } from '@apollo/react-hooks';
-import MapPage from './pages/Map';
-import StoreList from  './components/StoreList';
 import {apolloClient} from './client';
-import { NavigationContainer } from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Navigation from './containers/Navigation';
+/*
+    createMaterialTopTabNavigator = MaskStore map , list
+    createBottomTabNavigator = Mask, Corona, Board, options?
+    isAndroid? createDrawerNavigator : createbottomTabNavigator
+*/
 
 export default function App() {
-  // const Tab = createBottomTabNavigator();
-  const Stack = createStackNavigator();
   return (
     <ApolloProvider client={apolloClient}>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Map" component={MapPage}/>
-          <Stack.Screen name="List" component={StoreList}/>
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Navigation/>
     </ApolloProvider>
   );
 }
