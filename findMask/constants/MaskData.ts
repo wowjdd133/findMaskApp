55010cc2dabfa1d0e09cbb711d27c9acf7629312
wowjdd133 <wowjdd133@naver.com>
2020-06-17 19:36:30 +0900

    [Update] MaskData
    REMAIN_STAT 조금 변경.
    [key: string]: striing을 통해
    타입스크립트 오류 제거

--- findMask/constants/MaskData.ts
+++ findMask/constants/MaskData.ts
@@ -1,17 +1,21 @@
 export const LATITUDE_DELTA = 0.01501;
 
-export const MARKER_COLOR = {
-  "plenty": "#40E0D0",
-  "few": "#7CFC00",
-  "some": "#FFA500",
-  "empty": "#ff0000",
-  "break": "#808080",
+export interface ConstObject {
+  [key: string]: string
+}
+
+export const MARKER_COLOR:ConstObject = {
+  plenty: "#40E0D0",
+  few: "#7CFC00",
+  some: "#FFA500",
+  empty: "#ff0000",
+  break: "#808080",
 };
 
-export const REMAIN_STAT = {
-  plenty: "100개 이하",
-  few: "29개 이하",
-  some: "9개 이하",
+export const REMAIN_STAT:ConstObject = {
+  plenty: "100개 이상",
+  few: "30개이상 100개 미만",
+  some: "2개 이상 29개 미만",
   empty: "1개 이하",
   break: "판매 중지",
 };
