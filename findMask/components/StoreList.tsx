21fbea43580aae54cd6b4cb5ffb334ca4729861a
wowjdd133 <wowjdd133@naver.com>
2020-06-17 09:39:52 +0900

    [Update,Fix] StoreList
    Update -> 컴포넌트 분리, useState, useEffect를 이용해 검색하면 리렌더링 되게 만듦.
    Fix -> list rerendering할 때 키보드가 내려가는 버그 -> ListHaderComponent에 SearchBar 직접적으로 넣어줌.

--- findMask/components/StoreList.tsx
+++ findMask/components/StoreList.tsx
@@ -1,15 +1,17 @@
-import React, {useMemo} from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View,FlatList, SafeAreaView, TouchableOpacity} from 'react-native';
 import { useRoute } from '@react-navigation/native'
 import { MARKER_COLOR, REMAIN_STAT} from '../constants/MaskData';
 import { getDistanceFromLatLon, getElaspedTime} from '../utils/MaskUtil';
+import SearchBar from './SearchBar';
+import SearchItem from './SearchItem';
 
 interface StoreListProps {
   count: number;
   stores:Store[];
 }
 
-interface Store {
+export interface Store {
   code: string,
   addr: string,
   name: string,
@@ -26,79 +28,81 @@ interface Location {
   lng: number,
 }
 
-const StoreItem = React.memo<Store>((item) => {
-  
-    return(
-      <TouchableOpacity style={styles.item}>
-        <Text style={styles.name}>{item.name}</Text>
-        <Text style={{paddingTop: 5}}>{item.addr}</Text>
-        <Text style={{color: MARKER_COLOR[item.remain_stat], paddingTop: 5, fontSize: 17}}>마스크 수: {REMAIN_STAT[item.remain_stat]}</Text>
-        <View style={styles.flex}>
-          <Text>{item.elapsedTime}</Text>
-          <Text style={styles.distance}>{item.distance}km</Text>
-        </View>
-      </TouchableOpacity>
-    )  
-});
-
 const StoreList = () => {
 
- const Route = useRoute();
+  const [value, setValue] = useState('');
+  const [datas, setDatas] = useState<StoreListProps>();
+  
+  const Route = useRoute();
+  const data:StoreListProps = Route.params.data.Masks;
+  const location:Location = Route.params.location;
 
- const data:StoreListProps = Route.params.data.Masks;
- const location:Location = Route.params.location;
-
- console.log('list');
-
- data.stores.map((store) => {
-  store.distance = getDistanceFromLatLon(store.lat,location.lat,store.lng, location.lng);
-  store.elapsedTime = getElaspedTime(store.stock_at);
- })
-
- data.stores.sort((a,b) => {
-   if(a.distance && b.distance){
-    return a.distance < b.distance ? -1 : a.distance == b.distance ? 0 : 1
-   }
-   return a < b ? -1 : a == b ? 0 : 1;
- });
-
-  return(
-    <SafeAreaView>
-      <FlatList
-        data={data.stores}
-        keyExtractor={item => item.code}
-        renderItem={({item}) => (
-          <StoreItem {...item} />
-        )}
-      />
-    </SafeAreaView>
-  )
+  const searchFilter = (text:string) => {
+    if(datas){
+      const newData = data.stores.filter(item => {
+        return item.name.indexOf(text) > -1;
+      })
+    
+      setDatas({
+        count: datas.count,
+        stores: newData
+      });
+      setValue(text);
+    }
+    
 }
 
-const styles = StyleSheet.create({
-  item: {
-    marginVertical: 8,
-    marginHorizontal: 12,
-    padding: 10,
-    backgroundColor: '#FFFFFF',
-    borderRadius: 10,
-    fontSize: 13,
-  },
-  name: {
-    fontSize: 17,
-    fontWeight: '700'
-  },
-  distance: {
-    fontSize: 15,
-    fontWeight: '500',
-    color: '#00CCFF'
-  }
-  ,
-  flex: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    paddingVertical: 7
-  }
-})
+  const renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: '86%',
+          backgroundColor: '#CED0CE',
+          marginLeft: '14%'
+        }}
+      />
+    );
+  };
+
+  useEffect(() => {
+    if(data){
+      data.stores.map((store) => {
+        store.distance = getDistanceFromLatLon(store.lat,location.lat,store.lng, location.lng);
+        store.elapsedTime = getElaspedTime(store.stock_at);
+       })
+      
+       data.stores.sort((a,b) => {
+         if(a.distance && b.distance){
+          return a.distance < b.distance ? -1 : a.distance == b.distance ? 0 : 1
+         }
+         return a < b ? -1 : a == b ? 0 : 1;
+       });
+      
+       setDatas(data);
+     }
+  },[]);
+
+ return datas ? 
+ (<SafeAreaView>
+    <FlatList
+      data={datas.stores}
+      extraData={datas}
+      keyExtractor={item => item.code}
+      renderItem={({item}) => {
+        return(
+        <SearchItem {...item} />)
+      }}
+      ItemSeparatorComponent={renderSeparator}
+      ListHeaderComponent={
+        <SearchBar
+          placeholder="검색"
+          value={value}
+          onChangeText={searchFilter}
+        />
+      }
+    />
+  </SafeAreaView>): (<Text>hi</Text>)
+}
 
 export default StoreList;
