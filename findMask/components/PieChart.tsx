18cf71d09e7c7fde1b6d1a2903a6b4c137785576
wowjdd133 <wowjdd133@naver.com>
2020-06-19 09:12:39 +0900

    [Create] PieChart
    빅토리 파이 차트.

---
+++ findMask/components/PieChart.tsx
@@ -0,0 +1,39 @@
+import {VictoryPie, VictoryLabel} from 'victory-native';
+import { View, Dimensions, Text } from 'react-native';
+import {Svg} from 'react-native-svg';
+import * as React from 'react';
+
+interface ChartProps {
+  data: any | undefined;
+  x: string;
+  y: string;
+  text: string;
+}
+
+const Chart = ({data,x,y,text}:ChartProps) => {
+  return(
+    <Svg>
+      <VictoryPie
+        data={data}
+        x={x}
+        y={y}
+        width={(Dimensions.get('screen').width)}
+        innerRadius={110}
+        animate={{
+          duration: 2000
+        }}
+        padAngle={5}
+        children={<Text>hi</Text>}
+      />
+      <VictoryLabel
+        x={Dimensions.get('screen').width/2-45}
+        y={175}
+        style={{fontSize: 15}}
+        text={text}
+      />
+    </Svg>
+      
+  )
+}
+
+export default Chart;
