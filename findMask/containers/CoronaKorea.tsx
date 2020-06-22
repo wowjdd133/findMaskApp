57ff33e49b2435af08f9325b70792fbdf1042e27
wowjdd133 <wowjdd133@naver.com>
2020-06-22 14:50:12 +0900

    [Update[ CoronaKorea
    Update = 정렬

--- findMask/containers/CoronaKorea.tsx
+++ findMask/containers/CoronaKorea.tsx
@@ -6,7 +6,8 @@ import { Text, View, StyleSheet, ScrollView } from 'react-native';
 import KoreaIcon from '../svgs/Korea';
 import PieChart from '../components/PieChart';
 import BarChart from '../components/BarChart';
-import {GET_CORONA_KOREA_DATA} from '../querys/Corona'
+import { GET_CORONA_KOREA_DATA } from '../querys/Corona'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 interface CityType {
   countryName: string;
@@ -17,7 +18,7 @@ interface CityType {
 }
 
 export interface CoronaKoreaData {
-  CoronaKorea:{
+  CoronaKorea: {
     [key: string]: string | number;
     resultCode: string;
     TotalCase: string;
@@ -55,20 +56,20 @@ export interface CoronaKoreaData {
 }
 
 const CoronaKorea = () => {
-  const {loading,error,data} = useQuery<CoronaKoreaData>(
+  const { loading, error, data } = useQuery<CoronaKoreaData>(
     GET_CORONA_KOREA_DATA
   );
 
-  if(loading){
-    return <Loading/>
+  if (loading) {
+    return <Loading />
   }
 
-  if(error){
+  if (error) {
     console.error(error);
     return <Text>에러가..났어요..</Text>
   }
 
-  if(data && data.CoronaKorea){
+  if (data && data.CoronaKorea) {
     const coronaData = data.CoronaKorea
     let coronaCity = data.CoronaKoreaNew.data;
     coronaCity.shift();
@@ -80,73 +81,80 @@ const CoronaKorea = () => {
     })
 
     const CityList =
-          coronaCity.map((data) => {
-            const regex = /,/gi;
-            return(
-              <View key={data.countryName} 
-              style={{ 
-                width:"49%",
-                borderWidth:1,
-                height:175, 
-                borderRadius:25, 
-                alignItems:"center",
-                marginVertical:15,
-                justifyContent:"space-evenly"}}>
-                <Text style={{fontSize: 25}}>{data.countryName}</Text>
-                <View style={{borderRadius:25, 
-                  width:"95%",
-                  borderWidth:1,
-                  alignItems:"center",
-                  borderColor:"black",
-                  justifyContent:"center"}}>
-                  <Text style={{fontSize: 17}}>격리중: {Number(data.totalCase.replace(regex,''))-Number(data.death.replace(regex,''))-Number(data.recovered.replace(regex,''))}</Text>
-                </View>
-                <Text style={{fontSize: 17}}>총 확진자: {data.totalCase}</Text>
-                <Text style={{fontSize: 17}}>사망: {data.death}</Text>
-                <Text style={{fontSize: 17}}>격리해제: {data.recovered}</Text>
-              </View>
-            )
-          })
+      coronaCity.map((data) => {
+        const regex = /,/gi;
+        return (
+          <View key={data.countryName}
+            style={{
+              width: "49%",
+              borderWidth: 1,
+              height: 175,
+              borderRadius: 25,
+              alignItems: "center",
+              marginVertical: 15,
+              justifyContent: "space-evenly"
+            }}>
+            <Text style={{ fontSize: 25 }}>{data.countryName}</Text>
+            <View style={{
+              borderRadius: 25,
+              width: "95%",
+              borderWidth: 1,
+              alignItems: "center",
+              borderColor: "black",
+              justifyContent: "center"
+            }}>
+              <Text style={{ fontSize: 17 }}>격리중: {Number(data.totalCase.replace(regex, '')) - Number(data.death.replace(regex, '')) - Number(data.recovered.replace(regex, ''))}</Text>
+            </View>
+            <Text style={{ fontSize: 17 }}>총 확진자: {data.totalCase}</Text>
+            <Text style={{ fontSize: 17 }}>사망: {data.death}</Text>
+            <Text style={{ fontSize: 17 }}>격리해제: {data.recovered}</Text>
+          </View>
+        )
+      })
 
     return (
-      <ScrollView style={styles.container}>
-        <View style={styles.dataBox}>
-          <View style={{paddingLeft: 35}}>
-            <Text style={{fontSize:25}}>대한민국 총 확진자</Text>
-            <Text style={styles.titleText}>{coronaData.TotalCase}명</Text>
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.container}>
+          <View style={styles.dataBox}>
+            <View style={{ paddingLeft: 35 }}>
+              <Text style={{ fontSize: 25 }}>대한민국 총 확진자</Text>
+              <Text style={styles.titleText}>{coronaData.TotalCase}명</Text>
             </View>
-          <KoreaIcon size={164} color={"#ababab"}/>
-        </View>
-        <View style={styles.dataBox}>
-            <Text style={{fontSize:25}}>사망</Text>
-            <Text style={styles.titleText}>{coronaData.TotalDeath}명</Text>
-        </View>
-        <View style={styles.dataBox}>
-              <Text style={{fontSize:25}}>격리해제</Text>
-            <Text style={styles.titleText}>{coronaData.TotalRecovered}명</Text>
-        </View>
-        <View style={styles.dataBox}>
-          <BarChart
-            labels={["오늘 사망자","오늘 격리해제","오늘 확진자"]}
-            data={[parseInt(coronaData.TodayDeath), parseInt(coronaData.TodayRecovered), parseInt(koreaData.newCase)]}
-          />
-        </View>
-        <View style={styles.dataBox}>
-          {chartData === null ? <Text>확진자가 없습니다~!</Text>: <PieChart text={"오늘 확진자 비율"} data={chartData} x="countryName" y="newCase"/>}
-        </View>
-        <View style={styles.dataBox}>
-          <View style={{flex: 1, 
-            flexWrap: 'wrap', 
-            flexDirection:'row', 
-            justifyContent:'space-between'}}>
-            {CityList}
+            <KoreaIcon size={164} color={"#ababab"} />
           </View>
-        </View>
-      </ScrollView>
+          <View style={styles.dataBox}>
+            <Text style={{ fontSize: 25 }}>사망</Text>
+            <Text style={styles.titleText}>{coronaData.TotalDeath}명</Text>
+          </View>
+          <View style={styles.dataBox}>
+            <Text style={{ fontSize: 25 }}>격리해제</Text>
+            <Text style={styles.titleText}>{coronaData.TotalRecovered}명</Text>
+          </View>
+          <View style={styles.dataBox}>
+            <BarChart
+              labels={["오늘 사망자", "오늘 격리해제", "오늘 확진자"]}
+              data={[parseInt(coronaData.TodayDeath), parseInt(coronaData.TodayRecovered), parseInt(koreaData.newCase)]}
+            />
+          </View>
+          <View style={styles.dataBox}>
+            {chartData === null ? <Text>확진자가 없습니다~!</Text> : <PieChart text={"오늘 확진자 비율"} data={chartData} x="countryName" y="newCase" />}
+          </View>
+          <View style={styles.dataBox}>
+            <View style={{
+              flex: 1,
+              flexWrap: 'wrap',
+              flexDirection: 'row',
+              justifyContent: 'space-between'
+            }}>
+              {CityList}
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     )
   }
 
-  return <Loading/>
+  return <Loading />
 }
 
 const styles = StyleSheet.create({
