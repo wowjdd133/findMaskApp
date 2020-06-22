ab3d6843715a3dee43240f8c19ea117895a574c4
wowjdd133 <wowjddl133@naver.com>
2020-06-23 08:49:02 +0900

    [Update] server
    typeorm + type-graphql 로 변경

--- findMask/server/src/server.ts
+++ findMask/server/src/server.ts
@@ -1,40 +1,52 @@
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express';
-import depthLimit from 'graphql-depth-limit';
-import schema from './schema';
-import cors from 'cors';
-import { createServer } from 'http';
-import mongoose from 'mongoose';
-import 'dotenv/config';
+import "reflect-metadata";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import depthLimit from "graphql-depth-limit";
+// import schema from "./schema";
+import cors from "cors";
+import { createServer } from "http";
+import "dotenv/config";
+import { createConnection } from "typeorm";
+import {buildSchema} from 'type-graphql';
+import {UserResolver} from './resolver/UserResolver';
+import {BoardResolver} from './resolver/BoardResolver';
+import {ApolloContext} from './context/ApolloContext';
+
 // import {auth} from './middleware/auth';
 
 const corsOptions = {
-  origin: 'http://localhost:19006',
+  origin: "http://localhost:19006",
   credentials: true,
 };
-mongoose.Promise = global.Promise;
-//env로 바꾸기.. 현재 dotenv를 써도 오류가 뜸.
-mongoose.connect(process.env.MONGO_DB || "", { 
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useNewUrlParser: true,
-  useFindAndModify: false,}); 
 
-const app = express();
+createConnection()
+  .then(async () => {
+    const app = express();
 
-const server = new ApolloServer({
-  schema,
-  validationRules: [depthLimit(7)],
-});
+    const server = new ApolloServer({
+      context: ApolloContext,
+      schema: await buildSchema({
+        resolvers: [UserResolver,BoardResolver]
+      }),
+      validationRules: [depthLimit(7)],
+      tracing: true
+    });
 
-app.use('*', cors(corsOptions));
-// app.use((req,res,next) => {
-//   auth(req,res,next);
-// })
+    app.use("*", cors(corsOptions));
 
-server.applyMiddleware({app, path:'/graphql'});
+    server.applyMiddleware({ app, path: "/graphql" });
 
-const httpServer = createServer(app);
-httpServer.listen({port: 8000},
-  (): void => console.log('Server listening on port 8000'));
+    const httpServer = createServer(app);
+    httpServer.listen({ port: 8000 }, (): void =>
+      console.log("Server listening on port 8000")
+    );
+  })
+  .catch((err) => console.error(err));
 
+// mongoose.Promise = global.Promise;
+// //env로 바꾸기.. 현재 dotenv를 써도 오류가 뜸.
+// mongoose.connect(process.env.MONGO_DB || "", {
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useNewUrlParser: true,
+//   useFindAndModify: false,});
