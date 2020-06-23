9d385d156851f0f9c48c92dd9e7e1c868a941d0e
wowjdd133 <wowjddl133@naver.com>
2020-06-23 19:40:44 +0900

    [Update] User
    calss vaildator, name 추가.

--- findMask/server/src/entity/User.ts
+++ findMask/server/src/entity/User.ts
@@ -2,6 +2,9 @@ import { ObjectType, Field, ID,  } from 'type-graphql';
 import { Entity, PrimaryGeneratedColumn,Column, ObjectIdColumn, ObjectID, CreateDateColumn, BaseEntity, OneToMany,} from 'typeorm';
 import {Board} from './Board';
 import Permission from '../types/Permission';
+import { IsEmail } from 'class-validator';
+import { Comment } from './Comment';
+// import { Recomment } from './ReComment';
 
 @ObjectType()
 @Entity()
@@ -11,12 +14,17 @@ export class User extends BaseEntity{
   id: string;
 
   @Field(() => Permission)
-  @Column({type: 'varchar', length:5})
-  permission: Permission;
+  @Column({type: 'enum',enum: Permission,default:Permission.WORKER, nullable: true})
+  permission: string;
 
   @Field()
+  @IsEmail()
   @Column({type:'varchar', length: 100,unique: true })
   email: string;
+
+  @Field()
+  @Column({type:'varchar', length: 20 })
+  name: string;
 
   @Field()
   @Column({type:'varchar', length: 11,unique: true })
@@ -31,7 +39,15 @@ export class User extends BaseEntity{
   create_at: Date
 
   @Field(() => Board,{nullable:true})
-  @OneToMany(type => Board, board => board.uid,{nullable:true})
+  @OneToMany(type => Board, board => board.uid,{nullable:true,cascade: true})
   boards?: Board[];
   //board : objectID
+
+  @Field(() => Comment)
+  @OneToMany(type => Comment, comment => comment.author ,{nullable:true, cascade: true})
+  comments? : Comment[]
+
+  // @Field(() => Comment)
+  // @OneToMany(type => Recomment, recomment => recomment.author ,{nullable:true, cascade: true})
+  // recomments? : Recomment[]
 }
