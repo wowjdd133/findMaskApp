66a1033c8e340a9899272ce4f6639c2dfde5fcb2
wowjdd133 <wowjddl133@naver.com>
2020-06-23 15:42:29 +0900

    [Update] BoardResolver
    board arg 변경, delete, update 만듦, writeBoard 제대로 만듦.

--- findMask/server/src/resolver/BoardResolver.ts
+++ findMask/server/src/resolver/BoardResolver.ts
@@ -1,27 +1,93 @@
 import { Board } from "../entity/Board";
-import { Resolver, Query, Arg, Mutation } from "type-graphql";
+import { Resolver, Query, Arg, Mutation, Authorized, Ctx } from "type-graphql";
 import { User } from "../entity/User";
+import { ApolloContextInterface } from "../context/ApolloContext";
+import { ApolloError } from "apollo-server-express";
 
 @Resolver(Board)
 export class BoardResolver {
+  
   @Query((returns) => [Board])
   async boards() {
     return await Board.find();
   }
 
   @Query((retruns) => Board)
-  async board(@Arg("email") email: string) {
-    return await Board.find({ where: { email: email } });
+  async board(@Arg("id") id: string) {
+    return await Board.find({ where: { id: id } });
   }
 
+  @Authorized()
+  @Mutation(() => Boolean)
+  async deleteBoard(
+    @Ctx() apolloContext: ApolloContextInterface,
+    @Arg("id") id: string,
+  )
+  {
+    try{
+      const BoardData = await Board.findOne({id: id});
+
+      if(BoardData){
+        console.log(BoardData);
+        if(BoardData.uid.id === apolloContext.user.id){
+          BoardData.remove();
+  
+          return true;
+        }
+        throw new ApolloError("You have no authority")
+      }
+  
+      throw new ApolloError("invalid Board");
+    }catch(err){
+      console.error(err);
+      return false;
+    }
+  }
+
+  @Authorized()
+  @Mutation(() => Boolean)
+  async updateBoard(
+    @Arg('id') id:string,
+    @Arg('title') title: string,
+    @Arg('content') content:string,
+    @Ctx() {user}: ApolloContextInterface,
+  )
+  { 
+    try{
+      const BoardData = await Board.findOne({id});
+
+      if(BoardData){
+        if(BoardData.uid.id === user.id){
+          await Board.update({id: id}, {title: title, content: content});
+
+          return true;
+        }
+        throw new ApolloError("권한 없음.");
+      }
+      throw new ApolloError("invalid Board");
+    }catch(err){
+      console.error(err);
+      return false;
+    }
+  }
+
+  @Authorized()
   @Mutation(() => Boolean)
   async writeBoard(
     @Arg("title") title: string,
-    @Arg("content") content: string
+    @Arg("content") content: string,
+    @Ctx() apolloContext: ApolloContextInterface,
   ) {
     try {
+      const user = apolloContext.user;
+
+      if(!user){
+        throw new ApolloError("invalid token");
+      }
+      
       await Board.create({
         title,
+        uid: user,
         content,
       }).save();
 
