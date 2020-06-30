18b30fa65089172f0d4aa5d8c525578998d08e30
wowjdd133 <wowjddl133@naver.com>
2020-06-30 10:26:35 +0900

    [Update] WriteBoard
    글쓰기 기능 만듦.
    앞으로 글 쓰면 그 보드로 가게 해야함.

--- findMask/containers/WriteBoard.tsx
+++ findMask/containers/WriteBoard.tsx
@@ -1,11 +1,63 @@
-import * as React from 'react';
-import {View, Text} from 'react-native';
+import React, { useState } from 'react';
+import {SafeAreaView, GestureResponderEvent, Alert } from 'react-native';
+import TextInput from '../components/common/TextInput';
+import Card from '../components/common/Card';
+import Button from '../components/common/Button';
+import {WRITE_BOARD} from '../querys/Board';
+import { useMutation } from '@apollo/react-hooks';
 
 const WriteBoard = () => {
+
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [writeBoard, {data}] = useMutation(WRITE_BOARD);
+
+  if(data){
+    if(data.writeBoard){
+      //보드 보기로 변경하자 보드 만들고
+      Alert.alert("성공",data.writeBoard);
+    }
+  }
+
   return (
-    <View>
-      <Text>이곳은 글쓰기여</Text>
-    </View>
+    <SafeAreaView style={{flex:1, justifyContent:'center'}}>
+      <Card justifyContent="space-around">
+        <TextInput
+          height="10%"
+          placeholder="제목"
+          setValue={setTitle}
+          value={title}
+          maxLength={20}
+        />
+
+        <TextInput
+          height="50%"
+          placeholder="본문"
+          setValue={setContent}
+          value={content}
+          maxLength={400}
+          multiline
+          numOfLines={10}       
+        />
+        <Button
+          onPress={async (event: GestureResponderEvent) => {
+            try{
+              await writeBoard({
+                variables:{
+                  title: title,
+                  content: content
+                }
+              });
+            }catch(err){
+              Alert.alert("글쓰기 실패", err.message);
+            }
+            
+          }}
+          width="80%"
+          title="글쓰기"
+        />
+      </Card>
+    </SafeAreaView>
   )
 }
 
