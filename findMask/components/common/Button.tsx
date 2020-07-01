04f4919f1d0def9dbfcfaf7cee8dc98fbd7b7e46
wowjdd133 <wowjddl133@naver.com>
2020-07-01 13:25:43 +0900

    [Update] button
    touchopacity로 변경.

--- findMask/components/common/Button.tsx
+++ findMask/components/common/Button.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styled from 'styled-components/native';
 import { GestureResponderEvent, Button } from 'react-native';
+import TextC from './Text';
 
 interface ButtonProps extends ViewStyleProps {
   title: string;
-  color?: string;
   onPress: (event: GestureResponderEvent) => void;
   disabled?: boolean;
 }
@@ -12,24 +12,40 @@ interface ButtonProps extends ViewStyleProps {
 interface ViewStyleProps {
   marginTop?: number;
   width?: string;
+  stretch?: boolean;
+  color?: string;
+  backgroundColor: string;
+  around?: boolean;
 }
 
-const ViewStyle = styled.View`
+const ViewStyle = styled.TouchableOpacity`
   ${(props: ViewStyleProps) =>
     props.marginTop ? `marginTop: ${props.marginTop}px;` : ''
   }
   ${(props: ViewStyleProps) =>
+    props.around ? `borderRadius: 5px;` : ''
+  }
+  paddingHorizontal: 10px;
+  ${(props: ViewStyleProps) =>
     props.width ? `width: ${props.width};` : ''
   }
+  ${(props: ViewStyleProps) =>
+    props.stretch ? 'alignSelf: stretch;' : 'alignSelf: center;'
+  }
+  color: ${(props: ViewStyleProps) => props.color}
+  backgroundColor: ${(props: ViewStyleProps) => props.backgroundColor};
+  alignItems: center;
+  justifyContent: center;
 `
-const ButtonC = ({marginTop,width, ...props}: ButtonProps) => {
+
+const ButtonC = ({title,disabled,...props}: ButtonProps) => {
   return (
     <ViewStyle
-      marginTop={marginTop}
-      width={width}>
-      <Button
-        {...props}
-      />
+      {...props}
+      >
+        <TextC fontSize={24}>
+          {title}
+        </TextC>
     </ViewStyle>
   )
 }
