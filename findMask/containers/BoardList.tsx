c6d88d267e5d02c99805491ffa39642e424037c1
wowjdd133 <wowjddl133@naver.com>
2020-07-01 13:28:31 +0900

    [Update] BoardList
    boardlist만듦. query refetch를 위해 usefocusEffect 생명주기 사용.

--- findMask/containers/BoardList.tsx
+++ findMask/containers/BoardList.tsx
@@ -1,34 +1,105 @@
 import * as React from 'react';
-import {View, Text, Button} from 'react-native';
-
+import { Button, Alert, GestureResponderEvent } from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 import { useNavigation } from '@react-navigation/native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import CardC from '../components/common/Card';
+import { useQuery } from '@apollo/react-hooks';
+import { GET_BOARDS } from '../querys/Board';
+import Loading from '../components/Loading';
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
+import TextC from '../components/common/Text';
+import ButtonC from '../components/common/Button';
+
+interface BoardData {
+  boards: {
+    id: string;
+    title: string;
+    uid: {
+      email: string;
+      name: string;
+    }
+    create_at: Date;
+    update_at: Date;
+    viewCount: number;
+  }[];
+}
+
 const Board = () => {
+  //다시 돌아오면 업데이트 해야함.
   const navigation = useNavigation();
-  
-  return(
-    <View>
-      <Button
-        onPress={(e) => {
-          e.preventDefault();
-          navigation.navigate('Board')
-        }}
-        title="누르면 보드로!"
-      />
-      <Button
+  const { data, loading, error, refetch, networkStatus } = useQuery<BoardData>(GET_BOARDS, {
+    notifyOnNetworkStatusChange: true,
+  });
+
+  //다시 돌아오면 업데이트 함
+  useFocusEffect(
+    React.useCallback((): void => {
+      refetch();
+    }, [])
+  );
+
+  //refetch시에 실행.
+  if (networkStatus === 4) {
+    return (<Loading />);
+  }
+
+  if (loading) {
+    return (<Loading />);
+  }
+
+  if (error) {
+    Alert.alert("에러", error.message);
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <ButtonC
+        backgroundColor="#eeeeee"
+        color="#000000"
         onPress={(e) => {
           e.preventDefault();
           navigation.navigate('Login')
         }}
         title="누르면 로그인으로!"
       />
-      <Button
+      <ButtonC
+        backgroundColor="#eeeeee"
+        color="#000000"
         onPress={(e) => {
           e.preventDefault();
           navigation.navigate('WriteBoard')
         }}
         title="누르면 글쓰기로!"
       />
-    </View>
+      <CardC flex={1}>
+        <FlatList
+          style={{ backgroundColor: '#eeeeee', alignSelf: "stretch" }}
+          keyExtractor={item => item.id}
+          data={data!.boards}
+          renderItem={({ item }) => {
+            return (
+              <TouchableOpacity
+                onPress={(event: GestureResponderEvent) => {
+                  navigation.navigate("Board", {
+                    id: item.id
+                  });
+                }}
+                style={{ flex: 1 }}>
+                <CardC borderBottom={1}
+                  borderBottomColor="#ced4da">
+                  <TextC fontSize={32}>{item.title}</TextC>
+                  <TextC fontSize={16}>{item.viewCount}</TextC>
+                  <TextC fontSize={16}>{item.uid.name}</TextC>
+                  <TextC fontSize={16}>{item.create_at}</TextC>
+                  <TextC fontSize={16}>{item.update_at}</TextC>
+                </CardC>
+              </TouchableOpacity>
+            )
+          }}
+        />
+      </CardC>
+    </SafeAreaView>
   )
 }
 
