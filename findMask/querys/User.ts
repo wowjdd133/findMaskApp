8ff6404745796e6463c87e420aec1a0f5a3aab72
wowjdd133 <wowjddl133@naver.com>
2020-07-06 20:43:00 +0900

    [Update] User
    edit profile, comparepassword, user image추가.

--- findMask/querys/User.ts
+++ findMask/querys/User.ts
@@ -1,39 +1,73 @@
 import gql from "graphql-tag";
 
 export const LOGIN = gql`
-  mutation Login($email: String! $password: String!){
-    login(email: $email password: $password)
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password)
   }
-`
+`;
 
 export const REGISTER = gql`
-  mutation Register($email: String! $password: String! $phoneNumber: String! $name:String!){
-    register(email: $email password: $password phoneNumber: $phoneNumber name: $name)
+  mutation Register(
+    $email: String!
+    $password: String!
+    $phoneNumber: String!
+    $name: String!
+    $image: String
+  ) {
+    register(
+      email: $email
+      password: $password
+      phoneNumber: $phoneNumber
+      name: $name
+      image: $image
+    )
   }
-`
+`;
+export const COMPARE_PASSWORD = gql`
+  query comparePassword($password: String!) {
+    comparePassword(password: $password)
+  }
+`;
+
+export const EDIT_PROFILE = gql`
+  mutation updateProfile(
+    $id: String!
+    $name: String!
+    $phoneNumber: String!
+    $image: String
+  ) {
+    updateProfile(
+      id: $id
+      name: $name
+      phoneNumber: $phoneNumber
+      image: $image
+    )
+  }
+`;
 
 export const GET_USERS = gql`
-  query getUsers{
-    users{
+  query {
+    users {
       id
       email
       name
       phoneNumber
       create_at
       permission
+      image
     }
   }
-`
+`;
 
 export const GET_USER = gql`
-  query user{
-    user{
+  query {
+    user {
       id
       email
       name
-      phoneNumber
+      image
       create_at
-      permission
-    }   
+      phoneNumber
+    }
   }
-`
+`;
