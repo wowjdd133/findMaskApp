044bdff17a6d2e9f7798e078be49ba6bf6050cce
wowjdd133 <wowjddl133@naver.com>
2020-07-06 20:39:40 +0900

    [Create] ListItem

---
+++ findMask/components/ListItem.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { GestureResponderEvent } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import CardC from './common/Card';
+
+interface ListItemProp {
+  onPress: (e: GestureResponderEvent) => void;
+  children: React.ReactNode;
+}
+
+const ListItem = ({onPress, children}: ListItemProp) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={{ flex:1, alignSelf: 'stretch' }}
+    >
+      <CardC 
+        borderBottom={1}
+        borderBottomColor="#ced4da">
+        {children}
+      </CardC>
+    </TouchableOpacity>
+  )
+}
+
+export default ListItem;
