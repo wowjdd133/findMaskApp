8a169e0e57d496c846acfb3d85fa33ff8fcf6930
wowjdd133 <wowjddl133@naver.com>
2020-07-06 20:43:57 +0900

    [Update] register
    register 함수 분리, 컴포넌트 분리.

---
+++ findMask/components/register/Register.tsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import { SafeAreaView } from "react-native-safe-area-context"
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view"
+import TopComponent from "../common/TopComponent"
+import Close from '../../svgs/Close';
+import ButtonC from "../common/Button";
+import CardC from "../common/Card";
+import TextC from "../common/Text";
+import TextInputC from "../common/TextInput";
+import { GestureResponderEvent } from "react-native";
+
+interface RegisterProps {
+  name: string;
+  email: string;
+  phoneNumber: string;
+  password: string;
+  currentPassword: string;
+  setName: any;
+  setPhoneNumber: any;
+  setPassword: any;
+  setCurrentPassword: any;
+  setEmail: any;
+  handleGoBack: (e:GestureResponderEvent) => void;
+  handleOnPress: (e:GestureResponderEvent) => Promise<void>
+}
+
+const Register = (props:RegisterProps) => {
+
+  const {
+    name,
+    setName,
+    currentPassword,
+    handleGoBack,
+    handleOnPress,
+    password,
+    phoneNumber,
+    setCurrentPassword,
+    setPassword,
+    setPhoneNumber,
+    email,
+    setEmail
+  } = props;
+
+return (
+  <SafeAreaView style={{ flex: 1, backgroundColor: "#cccccc" }}>
+    <KeyboardAwareScrollView
+      keyboardShouldPersistTaps="always"
+      contentContainerStyle={{ flexGrow: 1 }}
+      scrollEnabled
+      resetScrollToCoords={{ x: 0, y: 0 }}
+    >
+      <TopComponent
+        paddingLeft={30}
+        onPress={handleGoBack}
+      >
+        <Close color="#000000" size={20} />
+        <ButtonC
+          backgroundColor="rgba(44,130,201,1)"
+          color="#aaaaaa"
+          onPress={handleOnPress}
+          title="완료"
+          width="30%"
+        />
+      </TopComponent>
+      <CardC height="50px">
+        <TextC fontSize={24}>
+          회원가입
+        </TextC>
+      </CardC>
+
+      <CardC flex={25} justifyContent="space-evenly">
+        <TextInputC
+          returnKeyType="next"
+          marginTop={20}
+          height="40px"
+          placeholder="이메일"
+          setValue={setEmail}
+          value={email}
+          keyboardType={"email-address"}
+          maxLength={20}
+        />
+        <TextInputC
+          marginTop={40}
+          height="40px"
+          placeholder="비밀번호"
+          setValue={setPassword}
+          value={password}
+          isPassword={true}
+          maxLength={20}
+        />
+         <TextInputC
+          marginTop={40}
+          height="40px"
+          placeholder="비밀번호 재확인"
+          setValue={setCurrentPassword}
+          value={currentPassword}
+          isPassword={true}
+          maxLength={20}
+        />
+        <TextInputC
+          marginTop={40}
+          height="40px"
+          placeholder="전화번호"
+          setValue={setPhoneNumber}
+          value={phoneNumber}
+          keyboardType={"phone-pad"}
+          maxLength={11}
+        />
+        <TextInputC
+          marginTop={40}
+          height="40px"
+          placeholder="이름"
+          setValue={setName}
+          value={name}
+          maxLength={12}
+        />
+      </CardC>
+    </KeyboardAwareScrollView>
+  </SafeAreaView>
+)
+}
+
+export default Register;
--- findMask/containers/Register.tsx
+++ findMask/containers/Register.tsx
@@ -1,21 +1,16 @@
 import React, { useState } from 'react';
-import Card from '../components/common/Card'
-import Text from '../components/common/Text'
-import { SafeAreaView } from 'react-native-safe-area-context';
-import TextInput from '../components/common/TextInput';
-import Button from '../components/common/Button';
+import Register from '../components/register';
 import { useMutation } from '@apollo/react-hooks';
 import { REGISTER } from '../querys/User';
 import { isEmail } from '../utils/loginUtil';
-import { Alert, KeyboardAvoidingView, Platform } from 'react-native';
+import { Alert, GestureResponderEvent } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import Close from '../svgs/Close';
 
-const Register = () => {
+const RegisterContainer = () => {
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [currentPassword, setcurrentPassword] = useState('');
+  const [currentPassword, setCurrentPassword] = useState('');
   const [name, setName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [register, { data }] = useMutation(REGISTER);
@@ -28,115 +23,59 @@ const Register = () => {
     }
   }
 
-  const goBack = () => {
+  const handleGoBack = (e: GestureResponderEvent) => {
     navigation.goBack();
   }
 
+  const handleOnPress = async (e: GestureResponderEvent) => {
+    e.preventDefault();
+    if (!isEmail(email)) {
+      Alert.alert("회원가입 실패", "이메일 확인.");
+      return;
+    } else if (phoneNumber.length < 11) {
+      Alert.alert("회원가입 실패", "휴대폰 확인. -를 제외한 숫자입니다.")
+      return;
+    } else if (password.length <= 8) {
+      Alert.alert("회원가입 실패", "비밀번호는 8자리를 넘겨주세요");
+      return;
+    } else if (password !== currentPassword) {
+      Alert.alert("회원가입 실패", "비밀번호가 일치하지 않습니다.");
+      return;
+    } else if (!name) {
+      Alert.alert("회원가입 실패", "이름을 확인해주세요.");
+      return;
+    }
+
+    try {
+      await register({
+        variables: {
+          email: email,
+          password: password,
+          phoneNumber: phoneNumber,
+          name: name
+        }
+      })
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#FFFFFF" }}>
-      <KeyboardAvoidingView
-        style={{ flex: 1 }}
-        keyboardVerticalOffset={100}
-        behavior={Platform.OS == "ios" ? "padding" : "height"}
-      >
-        <Card align="flex-start"
-          paddingLeft={30}
-          onPress={goBack}
-          touchable
-        >
-          <Close color="#000000" size={20} />
-        </Card>
-        <Card flex={10} justifyContent="space-around">
-          <Text fontSize={24}>
-            회원가입
-          </Text>
-          <TextInput
-            height="40px"
-            placeholder="이메일"
-            marginTop={30}
-            setValue={setEmail}
-            value={email}
-            keyboardType={"email-address"}
-            maxLength={20}
-          />
-          <TextInput
-            height="40px"
-            placeholder="비밀번호"
-            marginTop={30}
-            setValue={setPassword}
-            value={password}
-            isPassword={true}
-            maxLength={20}
-          />
-          <TextInput
-            height="40px"
-            placeholder="비밀번호 재확인"
-            marginTop={30}
-            setValue={setcurrentPassword}
-            value={currentPassword}
-            isPassword={true}
-            maxLength={20}
-          />
-          <TextInput
-            height="40px"
-            placeholder="전화번호"
-            marginTop={30}
-            setValue={setPhoneNumber}
-            value={phoneNumber}
-            keyboardType={"phone-pad"}
-            maxLength={11}
-          />
-          <TextInput
-            height="40px"
-            placeholder="이름"
-            marginTop={30}
-            setValue={setName}
-            value={name}
-            maxLength={12}
-          />
-
-          <Button
-            backgroundColor="rgba(44,130,201,1)"
-            color="#FFFFFF"
-            onPress={async () => {
-              if (!isEmail(email)) {
-                Alert.alert("회원가입 실패", "이메일 확인.");
-                return;
-              } else if (phoneNumber.length < 11) {
-                Alert.alert("회원가입 실패", "휴대폰 확인. -를 제외한 숫자입니다.")
-                return;
-              } else if (password.length <= 8) {
-                Alert.alert("회원가입 실패", "비밀번호는 8자리를 넘겨주세요");
-                return;
-              } else if (password !== currentPassword) {
-                Alert.alert("회원가입 실패", "비밀번호가 일치하지 않습니다.");
-                return;
-              } else if (!name) {
-                Alert.alert("회원가입 실패", "이름을 확인해주세요.");
-                return;
-              }
-
-              try {
-                await register({
-                  variables: {
-                    email: email,
-                    password: password,
-                    phoneNumber: phoneNumber,
-                    name: name
-                  }
-                })
-              } catch (err) {
-                console.log(err);
-              }
-            }}
-            title="회원가입"
-            marginTop={30}
-            width="90%"
-          />
-        </Card>
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+    <Register
+      currentPassword={currentPassword}
+      handleGoBack={handleGoBack}
+      email={email}
+      setEmail={setEmail}
+      handleOnPress={handleOnPress}
+      name={name}
+      password={password}
+      phoneNumber={phoneNumber}
+      setCurrentPassword={setCurrentPassword}
+      setName={setName}
+      setPassword={setPassword}
+      setPhoneNumber={setPhoneNumber}
+    />
   )
 }
 
-export default Register;
+export default RegisterContainer;
