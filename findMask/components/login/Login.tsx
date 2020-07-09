6777fce5955d97ac85fc017f7381753179440c91
wowjdd133 <wowjddl133@naver.com>
2020-07-09 20:06:32 +0900

    [Update] Login
    margin Top, keyboardAwreScrollView

--- findMask/components/login/Login.tsx
+++ findMask/components/login/Login.tsx
@@ -7,6 +7,8 @@ import ButtonC from '../common/Button';
 import TextC from '../common/Text';
 import TextInputC from '../common/TextInput';
 import { GestureResponderEvent } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import KeyboardAwareScrollViewComponent from '../common/KeyboardAwareScrollView';
 
 interface LoginProps {
   email: string;
@@ -34,6 +36,7 @@ const Login = (props: LoginProps) => {
   } = props;
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#FFFFFF" }}>
+      <KeyboardAwareScrollViewComponent>
         <TopComponent
           paddingLeft={30}
           onPress={goBack}
@@ -47,11 +50,12 @@ const Login = (props: LoginProps) => {
             width="30%"
           />
         </TopComponent>
-        <CardC flex={5} justifyContent="space-around">
-          <TextC fontSize={30} marginTop={30}>
+        <CardC flex={1}>
+          <TextC fontSize={30} marginTop={50}>
             Find Mask App
           </TextC>
           <TextInputC
+            marginTop={40}
             keyboardType="email-address"
             maxLength={25}
             height="45px"
@@ -60,6 +64,7 @@ const Login = (props: LoginProps) => {
             placeholder="Email"
           />
           <TextInputC
+            marginTop={40}
             height="45px"
             maxLength={15}
             setValue={setPassword}
@@ -70,6 +75,7 @@ const Login = (props: LoginProps) => {
           />
           <ButtonC
             around
+            marginTop={40}
             backgroundColor="rgba(44,130,201,1)"
             color="#FFFFFF"
             onPress={handleOnPress}
@@ -77,6 +83,7 @@ const Login = (props: LoginProps) => {
           />
           <ButtonC
             around
+            marginTop={40}
             backgroundColor="rgba(44,130,201,1)"
             color="#FFFFFF"
             onPress={handleLogin}
@@ -85,6 +92,7 @@ const Login = (props: LoginProps) => {
             disabled={loginState}
           />
         </CardC>
+      </KeyboardAwareScrollViewComponent>
     </SafeAreaView>
   )
 }
