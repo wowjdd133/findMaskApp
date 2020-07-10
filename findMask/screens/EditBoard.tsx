9a4e35c9ca07c3b96f03b7a08ef42d26ce0d17ba
wowjdd133 <wowjddl133@naver.com>
2020-07-10 22:07:41 +0900

    [Create] editboard screen

---
+++ findMask/screens/EditBoard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import EditBoardContainer from '../containers/EditBoard';
+
+const EditBoardScreen = () => {
+  return (
+    <EditBoardContainer/>
+  )
+}
+
+export default EditBoardScreen;
--- findMask/screens/index.ts
+++ findMask/screens/index.ts
@@ -9,6 +9,7 @@ import LoginScreen from './Login'
 import RegisterScreen from './Register';
 import WriteBoardScreen from './WriteBoard';
 import EditProfileScreen from './EditrProfile';
+import EditBoardScreen from './EditBoard';
 
 export const screens = {
   BoardScreen,
@@ -21,5 +22,6 @@ export const screens = {
   RegisterScreen,
   BoardListScreen,
   WriteBoardScreen,
-  EditProfileScreen
+  EditProfileScreen,
+  EditBoardScreen
 };
